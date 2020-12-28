966b639aee4ec87e53025478f1b32198184a72f6
georgebejinariu <44533474+georgebejinariu@users.noreply.github.com>
2020-12-28 16:55:07 +0100

    Add files via upload

--- time_comparison.py
+++ time_comparison.py
@@ -5,14 +5,15 @@ Created on Sun Dec 27 15:19:55 2020
 @author: Asus
 """
 from Graph_Class import Graph
+from Graph_Class import con_graf
 from Search_optimal import f_lit_con_graf
 from Search_optimal import search_min
-from Search_optimal import naive_search
+from Search_optimal import simple_search
 
 import random
 import timeit
 
-g = f_lit_con_graf(100,220) 
+g = con_graf(400,600) 
 
 
 
@@ -20,37 +21,42 @@ g = f_lit_con_graf(100,220)
 code_to_test1 = """
 
 from Graph_Class import Graph
+from Graph_Class import con_graf
 from Search_optimal import f_lit_con_graf
 from Search_optimal import search_min
-from Search_optimal import naive_search
+from Search_optimal import simple_search
 
 import random
 
-g = f_lit_con_graf(300,1000) 
+g = con_graf(400,600) 
 
-naive_search(g)
+simple_search(g)
 
 """
 
 code_to_test2 = """
 
 from Graph_Class import Graph
+from Graph_Class import con_graf
 from Search_optimal import f_lit_con_graf
 from Search_optimal import search_min
-from Search_optimal import naive_search
+from Search_optimal import simple_search
 
 import random
 
-g = f_lit_con_graf(300,1000) 
+g = con_graf(400,600) 
 
 search_min(g)
 
 """
 
-elapsed_time1 = timeit.timeit(code_to_test1, number=10)/10
-elapsed_time2 = timeit.timeit(code_to_test2, number=10)/10
+elapsed_time1 = timeit.timeit(code_to_test1, number=1)/1
+elapsed_time2 = timeit.timeit(code_to_test2, number=1)/1
 
 
-print(elapsed_time1, 'vs', elapsed_time2)
+print('Simple method',elapsed_time1,'s', 'vs', elapsed_time2,'s','Improved method')
+
+
+
 
 
